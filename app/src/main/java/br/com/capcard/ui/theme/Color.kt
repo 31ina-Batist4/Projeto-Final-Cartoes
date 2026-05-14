1e723368790350dbf263d548e8b5d4c585b97cb8
ELINA BATISTA <elinabatista_@hotmail.com>
2026-05-14 17:45:26 -0300

    paleta de cores

--- app/src/main/java/br/com/capcard/ui/theme/Color.kt
+++ app/src/main/java/br/com/capcard/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package br.com.capcard.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val AzulEscuro = Color(0x0D47A1)
+val AzulMedio = Color(0x1976D2)
+val AzulClaro = Color(0x42A5F5)
+val AzulSuave= Color(0x90CAF9)
+val Branco = Color(0xFFFFFF)
+val Gelo = Color(0xF5F7FB)
+val Texto = Color(0x7B8BA1)
